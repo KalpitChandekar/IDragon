7636da34d4948bddb3b95d27522be3ec0d2fcb59
Kalpit Chandekar <kalpitchandekar1736@gmail.com>
2023-09-28 20:23:25 +0530

    game over after collision

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-document.onkeydown = function(e) {
-    if (e.keyCode ===38){
-        dino= document.querySelector('.dino');
-        dino.classList.add('animateDino');
-        setTimeout(() => {
-            dino.classList.remove('animateDino');
-        }, 700);
-    }
+document.onkeydown = function (e) {
+  if (e.keyCode === 38) {
+    dino = document.querySelector(".dino");
+    dino.classList.add("animateDino");
+    setTimeout(() => {
+      dino.classList.remove("animateDino");
+    }, 700);
+  }
 };
+
+setInterval(() => {
+  dino = document.querySelector(".dino");
+  gameOver = document.querySelector(".gameOver");
+
+  obstacle = document.querySelector(".obstacle");
+
+  dx = parseInt(window.getComputedStyle(dino, null).getPropertyValue("left"));
+  dy = parseInt(window.getComputedStyle(dino, null).getPropertyValue("top"));
+
+  ox = parseInt(
+    window.getComputedStyle(obstacle, null).getPropertyValue("left")
+  );
+  oy = parseInt(
+    window.getComputedStyle(obstacle, null).getPropertyValue("top")
+  );
+
+  offsetX = Math.abs(dx - ox);
+  offsetY = Math.abs(dy - oy);
+
+
+  if (offsetX < 93 && offsetY < 52) {
+    gameOver.style.visibility = "visible";
+    obstacle.classList.remove("obstacleAni");
+  }
+}, 100);
